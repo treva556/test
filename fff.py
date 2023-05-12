b3dec4b975345c093a3bf50c57f63079448bcb18
treva556 <marktreva92@gmail.com>
2023-05-12 04:05:10 +0300

    linear search

--- fff.py
+++ fff.py
@@ -1,1 +1,12 @@
 print("helo")
+
+## linear search
+print("Hello, World!")
+
+def linearSearch(ls, value):
+    for i in range(len(ls)):
+        if ls[i] == value:
+            return i
+        
+    return -1
+
